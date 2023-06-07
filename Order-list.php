4329acec4465e4840475693dff4dbf92dd4afb4b
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2023-06-07 11:39:04 +0330

    menu for dashbaord

--- Order-list.php
+++ Order-list.php
@@ -6,34 +6,41 @@ Description: This plugin allows you to have own orders list to show
 Author: Parsa Mirzaie
 Version: 1.0.0
 Author URI: https://eskanogroup.ir
-Text Domain: Social
+Text Domain: wc-order-list
 License: GPL2
 Developers => 
           Parsa Mirzaie -> PHP Developer : https://github.com/Parsa-mrz/
 */
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 class OrderList
 {
-  public function __construct()
-  {
-    $this->define_constant();
-    $this->init();
-  }
-  public function activation()
-  {
-  }
-  public function deactivation()
-  {
-  }
-  public function define_constant()
-  {
-    define("SOCL_DIR", plugin_dir_path(__FILE__));
-    define("SOCL_URL", plugin_dir_url(__FILE__));
-  }
-  public function init()
-  {
-    register_activation_hook(__FILE__, [$this, 'activation']);
-    register_deactivation_hook(__FILE__, [$this, 'deactivation']);
-  }
+    public function __construct()
+    {
+        $this->define_constant();
+        $this->init();
+    }
+    public function activation()
+    {
+    }
+    public function deactivation()
+    {
+    }
+    public function define_constant()
+    {
+        define("ORD_LI_DIR", plugin_dir_path(__FILE__));
+        define("ORD_LI_URL", plugin_dir_url(__FILE__));
+    }
+    public function init()
+    {
+        register_activation_hook(__FILE__, [$this, 'activation']);
+        register_deactivation_hook(__FILE__, [$this, 'deactivation']);
+        require_once(ORD_LI_DIR . DIRECTORY_SEPARATOR . 'vendor/autoload.php');
+        $this->add_classes();
+    }
+
+    public function add_classes()
+    {
+        require_once(ORD_LI_DIR . DIRECTORY_SEPARATOR . 'App/Dashboard/Menu.php');
+    }
 }
 new OrderList();
---
+++ src/templates/dashboard/menu.view.php
@@ -0,0 +1,3 @@
+<div class="wrap">
+    <h1><?php _e('Order List','wc-order-list') ?></h1>
+</div>
